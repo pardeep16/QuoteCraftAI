7e596a5bfa9ca5dbd80810051afb73d88ebd8635
Pardeep Kumar <pardeepkr@Pardeeps-MacBook-Air-2.local>
2026-04-28 07:13:32 +0530

    LLM Integration

--- frontend/quotecraft-ui/src/api/quote.ts
+++ frontend/quotecraft-ui/src/api/quote.ts
@@ -3,24 +3,16 @@ import type { Quote } from "../types";
 
 
 export const generateQuoteWithImage = async(data:any): Promise<Quote> => {
-    const token = localStorage.getItem('quotecraft_access_token');
       const res = await api.post<Quote>('/quotes/image-generate', {
-        headers: {
-          Authorization: `Bearer ${token}`
-        },
-        data
+        ...data
       });
 
       return res.data;
 }
 
 export const generateQuote = async(data:any): Promise<Quote> => {
-    const token = localStorage.getItem('quotecraft_access_token');
       const res = await api.post<Quote>('/quotes/generate', {
-        headers: {
-          Authorization: `Bearer ${token}`
-        },
-        data
+        ...data
       });
 
       return res.data;
